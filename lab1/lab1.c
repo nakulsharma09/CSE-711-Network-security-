c501f1d4d9a8a22f73eb8be6ed587c4105a4e56c
nakulsharma09 <nakul.57-cse-19@mietjammu.in>
2022-10-31 21:12:43 +0530

    first commit

---
+++ lab1/lab1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+    char message[100], ch;
+    int i, key;
+    printf("Enter a message to encrypt: ");
+    gets(message);
+    printf("Enter key: ");
+    scanf("%d", &key);
+    for (i = 0; message[i] != '\0'; ++i)
+    {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch + key;
+            message[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch + key;
+            message[i] = ch;
+        }
+    }
+
+    printf("Encrypted message: %s", message);
+
+    for (i = 0; message[i] != '\0'; ++i)
+    {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch - key;
+            if (ch < 'a')
+            {
+                ch = ch + 'z' - 'a' + 1;
+            }
+            message[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch - key;
+            if (ch < 'A')
+            {
+                ch = ch + 'Z' - 'A' + 1;
+            }
+            message[i] = ch;
+        }
+    }
+    printf("\nDecrypted message: %s\n", message);
+    return 0;
+}
